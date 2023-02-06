977b2d53ae4b890a5e1bd42e36f8b4d20f9de8e8
Lucas <lucas.robert@lepoles.org>
2023-02-06 11:19:02 +0100

    4eme push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,11 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(ProduitRepository $produitRepository): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'listeProduits' => $produitRepository->findAll()
-        ]);
-    }
+    #[Route('/', name: 'app_home')]
+    // Cette fonction publique s'appelle "index" et prend en paramètre un objet "ProduitRepository"
+public function index(ProduitRepository $produitRepository): Response
+{
+    // Elle renvoie la vue "home/index.html.twig" avec un tableau associatif de données
+    return $this->render('home/index.html.twig', [
+        // La clé "listeProduits" contiendra tous les produits trouvés en utilisant la méthode "findAll" de l'objet "ProduitRepository"
+        'listeProduits' => $produitRepository->findAll()
+    ]);
+}
+
 }
--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -23,88 +23,108 @@ class PanierController extends AbstractController
     /**
      * @Route("/", name="app_panier")
      */
-    public function index(Session $session): Response
-    {
-        $panier = $session->get("panier", []);
-        return $this->render('panier/index.html.twig', [
-            'panier' => $panier,
-        ]);
-    }
+    // Cette fonction publique s'appelle "index" et prend en paramètre un objet "Session"
+public function index(Session $session): Response
+{
+    // On récupère la valeur de la clé "panier" dans la session, ou un tableau vide si la clé n'existe pas
+    $panier = $session->get("panier", []);
+    // Elle renvoie la vue "panier/index.html.twig" avec un tableau associatif de données
+    return $this->render('panier/index.html.twig', [
+        // La clé "panier" contiendra la valeur récupérée de la session
+        'panier' => $panier,
+    ]);
+}
+
 
 
     /**
      * @Route("/ajouter-produit-{id}", name="app_panier_ajouter", requirements={"id"="\d+"})
      */
-    public function ajouter($id, ProduitRepository $pr, Session $session, Request $rq)
-    {
-        /**
-         L'objet de la classe Request contient toutes les valeurs des superglobales de PHP. Pour chaque superglobales, il y a une propriété de $rq qui
-         correspond : 
-            $rq->query     <=>     $_GET
-            $rq->request   <=>     $_POST
-            $rq->files     <=>     $_FILES
-            ...
-            Ces propriétés sont des objets, sur lesquels on peut utiliser les méthodes 
-                get('indice'), has(...)
-         */
-        $quantite = $rq->query->get("qte", 1) ?: 1;
-        $produit = $pr->find($id);
-        $panier = $session->get("panier", []); // on récupère ce qu'il y a dans le panier en session
-
-        $produitDejaDansPanier = false;
-        foreach ($panier as $indice => $ligne) {
-            if ($produit->getId() == $ligne["produit"]->getId()) {
-                $panier[$indice]["quantite"] += $quantite;
-                $produitDejaDansPanier = true;
-                break;  // pour sortir de la boucle foreach
-            }
+    // Cette fonction publique s'appelle "ajouter" et prend en paramètre l'identifiant du produit, un objet "ProduitRepository", un objet "Session", et un objet "Request"
+public function ajouter($id, ProduitRepository $pr, Session $session, Request $rq)
+{
+    // On récupère la quantité dans la requête (query), sinon la quantité par défaut est 1
+    $quantite = $rq->query->get("qte", 1) ?: 1;
+    // On récupère le produit à partir de l'identifiant et de l'objet "ProduitRepository"
+    $produit = $pr->find($id);
+    // On récupère ce qui est déjà présent dans le panier en session
+    $panier = $session->get("panier", []);
+    
+    // On définit une variable pour savoir si le produit est déjà dans le panier
+    $produitDejaDansPanier = false;
+    // Pour chaque ligne du panier
+    foreach ($panier as $indice => $ligne) {
+        // Si le produit actuel a le même identifiant que celui dans la ligne
+        if ($produit->getId() == $ligne["produit"]->getId()) {
+            // On augmente la quantité pour cette ligne
+            $panier[$indice]["quantite"] += $quantite;
+            // On marque que le produit est déjà dans le panier
+            $produitDejaDansPanier = true;
+            break;  // pour sortir de la boucle foreach
         }
-        if (!$produitDejaDansPanier) {
-            $panier[] = ["quantite" => $quantite, "produit" => $produit];  // on ajoute une ligne au panier => $panier est un array d'array
-        }
-
-
-        $session->set("panier", $panier);  // je remets $panier dans la session, à l'indice 'panier'
-        //dd($produit); // dd : Dump and Die
-        
-
-        //return $this->redirectToRoute("app_home");
-        $nb = 0;
-        foreach ($panier as $ligne){
-            $nb += $ligne["quantite"];
-        }
-
-
-        return $this->json($nb);
     }
+    // Si le produit n'est pas déjà dans le panier
+    if (!$produitDejaDansPanier) {
+        // On ajoute une nouvelle ligne au panier en indiquant la quantité et le produit
+        $panier[] = ["quantite" => $quantite, "produit" => $produit];
+    }
+
+    // On remet le panier dans la session à l'indice 'panier'
+    $session->set("panier", $panier);  
+
+    // On compte le nombre total de produits dans le panier
+    $nb = 0;
+    foreach ($panier as $ligne){
+        $nb += $ligne["quantite"];
+    }
+
+    // On renvoie le nombre total de produits dans le panier en format JSON
+    return $this->json($nb);
+}
+
 
     /** 
      * @Route("/vider", name="app_panier_vider")
      */
 
-    public function vider(Session $session)
-    {
-        $session->remove("panier");
-        return $this->redirectToRoute("app_panier");
-    }
+   //Fonction pour vider le panier
+public function vider(Session $session)
+{
+    //Suppression de la variable 'panier' de la session
+    $session->remove("panier");
+    
+    //Redirection vers la page du panier
+    return $this->redirectToRoute("app_panier");
+}
+
 
 
     /** 
      * @Route("/supprimer-produit-{id}", name="app_panier_supprimer", requirements={"id"="\d+"})
      */
 
-    public function supprimer(Produit $produit, Session $session)
-    {
-        $panier = $session->get("panier", []);
-        foreach ($panier as $indice => $ligne) {
-            if ($ligne['produit']->getId() == $produit->getId()) {
-                unset($panier[$indice]);
-                break;
-            }
+    //Fonction pour supprimer un produit du panier
+public function supprimer(Produit $produit, Session $session)
+{
+    //Récupération du panier en session
+    $panier = $session->get("panier", []);
+    
+    //Parcours des produits dans le panier pour trouver celui à supprimer
+    foreach ($panier as $indice => $ligne) {
+        if ($ligne['produit']->getId() == $produit->getId()) {
+            //Suppression du produit
+            unset($panier[$indice]);
+            break;
         }
-        $session->set("panier", $panier);
-        return $this->redirectToRoute("app_panier");
     }
+    
+    //Mise à jour du panier dans la session
+    $session->set("panier", $panier);
+    
+    //Redirection vers la page du panier
+    return $this->redirectToRoute("app_panier");
+}
+
 
     /** 
      * @Route("/valider", name="app_panier_valider")
